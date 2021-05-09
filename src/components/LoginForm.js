952923441619e8a58a14ffd48960fa5bbd823a46
Brinton <brintonfoyreed@gmail.com>
2021-05-09 09:58:48 -0700

    commit tree 45dfd3163c2b04dbbe5aceaa2ea087fe2de5e019

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,54 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
 
+  handleChanges = e => {
+    this.setState ({
+      [e.target.name]: e.target.value
+  }
+    )
+}
+
+  submitChanges = event => {
+    event.preventDefault()
+    const { username, password } = this.state;
+    console.log('LoginForm', event, username, password);
+    if (!username || !password) return;
+    // this.props.handleLogin(this.state.username, this.state.password)  //!Why doesn't work?!
+    this.props.handleLogin(this.state);
+  }
+  
   render() {
+    
+    const { username, password } = this.state;
+    const isEnabled = username.length > 0 && password.length > 0;
+
     return (
-      <form>
+      <form onSubmit={event => this.submitChanges(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" 
+              onChange={e => this.handleChanges(e)}
+              value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+              onChange={e => this.handleChanges(e)}
+              value={this.state.password}/>
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" 
+          disabled={!isEnabled}>Submit</button>
         </div>
       </form>
     );
